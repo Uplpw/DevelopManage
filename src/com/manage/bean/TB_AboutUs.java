abd999cd4324a04b2162cd9de8b5be9f8c99638c
董千汇 <48642201+DqHhhh@users.noreply.github.com>
2019-05-22 08:05:08 +0800

    餐厅公告管理模块所用bean
    TB_AboutUs.java

---
+++ src/com/manage/bean/TB_AboutUs.java
@@ -0,0 +1,28 @@
+package com.manage.bean;
+
+public class TB_AboutUs {
+	
+	
+	private String name;
+	private String content;
+
+	
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}	
+}
+	
+	
